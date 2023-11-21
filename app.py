59842874fa9df9dcdae3288ea2587f09164304ea
Felipe Haddad <felipehaddad25@gmail.com>
2023-11-21 17:56:16 -0300

    finalização das funções de hardware

--- app.py
+++ app.py
@@ -1,41 +1,41 @@
-from flask import Flask, request, render_template, redirect, flash, url_for
-from decimal import Decimal, InvalidOperation
-import re
+from flask import Flask, request, render_template, redirect, flash
 from init_db import conectarBD
 import datetime
+
+from validations import is_date, is_decimal, number_length, validate_date, validate_id, validate_price, is_in_list, validate_patrimony
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'fuabsnfouasbf0384h230br23082308328rftb32i230trg32t3gb20tg23tb'
 
 
-def is_decimal(n):
-    try:
-        Decimal(n)
-        return True
-    except InvalidOperation:
-        return False
+# def is_decimal(n):
+#     try:
+#         Decimal(n)
+#         return True
+#     except InvalidOperation:
+#         return False
 
 
-def is_date(value):
-    date_regex = re.compile(r'^\d{4}-\d{2}-\d{2}$')
+# def is_date(value):
+#     date_regex = re.compile(r'^\d{4}-\d{2}-\d{2}$')
 
-    if date_regex.match(value):
-        return True
-    else:
-        return False
+#     if date_regex.match(value):
+#         return True
+#     else:
+#         return False
 
 
-def number_length(number, n):
-    # converte o numero em string
-    number_str = str(number)
-    # encontra o tamanho da string e subtrai 1 para desconsiderar o ponto decimal
-    number_length = len(number_str)
-    # se o tamanho for maior que n ou menor ou igual que 0, então retorna false
-    if number_length > n or number_length <= 0:
-        return False
-    else:
-        return True 
+# def number_length(number, n):
+#     # converte o numero em string
+#     number_str = str(number)
+#     # encontra o tamanho da string e subtrai 1 para desconsiderar o ponto decimal
+#     number_length = len(number_str)
+#     # se o tamanho for maior que n ou menor ou igual que 0, então retorna false
+#     if number_length > n or number_length <= 0:
+#         return False
+#     else:
+#         return True 
 
 def money_format(value):
     return f"R${value:,.2f}"
@@ -48,6 +48,7 @@ def convert_to_date(value):
 
 app.jinja_env.filters["money_format"] = money_format
 
+
 DEPARTMENTS_LIST = [
     "Atendimento ao Cliente",
     "Contabilidade",
@@ -58,7 +59,7 @@ DEPARTMENTS_LIST = [
     "Recursos Humanos",
     "Reserva",
     "Sala de Reuniões",
-    "TI",
+    "T.I.",
     "Vendas"
 ]
 
@@ -88,6 +89,8 @@ def after_request(response):
 def index():
     return render_template('index.html')
 
+
+# HARDWARES
 @app.route("/add_hardware", methods=["POST","GET"])
 def add_hardware():
     if request.method == "POST":
@@ -100,7 +103,7 @@ def add_hardware():
         
 
         # VALIDAR SE patrimony EXISTE, POSSUI 6 DIGITOS NUMERICOS
-        if not patrimony or not number_length(patrimony, 6):
+        if not validate_patrimony(patrimony, 6):
             flash("Patrimonio deve conter seis dígitos numéricos!", "warning")
             return render_template("add_hardware.html", departments=DEPARTMENTS_LIST)
         
@@ -128,12 +131,12 @@ def add_hardware():
             
 
         # VALIDAR SE A dt_buy EXISTE E SE ESTÁ NO FORMATO DE DATA
-        if not dt_buy or not is_date(dt_buy):
+        if not validate_date(dt_buy):
             flash("Informe a data de compra do ativo!", "warning")
             return render_template("add_hardware.html", departments=DEPARTMENTS_LIST)
 
         # VALIDAR SE A dt_pr_rev EXISTE E SE ESTÁ NO FORMATO DE DATA
-        elif not dt_pr_rev or not is_date(dt_pr_rev):
+        elif not validate_date(dt_pr_rev):
             flash("Informe a próxima data de revisão do ativo!", "warning")
             return render_template("add_hardware.html", departments=DEPARTMENTS_LIST)
         
@@ -149,13 +152,13 @@ def add_hardware():
 
 
         #  VALIDAR SE O price POSSUI 7 DIGITOS NUMERICOS, SE É UM float E SE EXISTE
-        if not price or not number_length(price, 8) or not is_decimal(price):
+        if not validate_price(price, 8):
             flash("Preço inicial do ativo deve ser um número decimal positivo com no máximo 7 dígitos!", "warning")
             return render_template("add_hardware.html", departments=DEPARTMENTS_LIST)
 
 
         #  VALIDAR SE O department ESTA DENTRO DA LISTA DE DEPARTAMENTOS DEFINIDA
-        if department not in DEPARTMENTS_LIST or not department:
+        if not is_in_list(department, DEPARTMENTS_LIST):
             flash("Departamento não encontrado", "danger")
             return redirect("/add_hardware")
     
@@ -175,7 +178,6 @@ def add_hardware():
     else: #GET
         return render_template("add_hardware.html", departments=DEPARTMENTS_LIST)
     
-# HARDWARES
 @app.route("/all_hardwares")
 def all_hardwares():
     # consultar todos os hardwares
@@ -219,8 +221,9 @@ def delete_hardware():
     hardware_id = request.form.get("hardware_id")
 
     # validar se o hardware_id existe, se é um digito e se é maior que 0
-    if not hardware_id or not hardware_id.isdigit() or int(hardware_id) <= 0:
-        flash("1 Hardware não encontrado!", "danger")
+    if not validate_id(hardware_id):
+    # if not hardware_id or not hardware_id.isdigit() or int(hardware_id) <= 0:
+        flash("Hardware não encontrado!", "danger")
         return redirect("/all_hardwares")
     
     # validar se o hardware escolhido está no banco de dados
@@ -262,7 +265,8 @@ def edit_hardware():
         department = request.form.get("department")
 
         # VALIDAR SE o ID EXISTE
-        if not hardware_id or int(hardware_id) < 1:
+        # if not hardware_id or int(hardware_id) < 1:
+        if not validate_id(hardware_id):
             flash("3 Hardware não encontrado 1!", "danger")
             return redirect("/all_hardwares")
         
@@ -277,7 +281,8 @@ def edit_hardware():
         counter = 0
         
         # VALIDAR SE DT_BUY EXISTE E ESTÁ EM FORMATO DATE
-        if dt_buy and not is_date(dt_buy):
+        # if dt_buy and not is_date(dt_buy):
+        if not validate_date(dt_buy):
             cursor.close() 
             connection.close()
             flash("Data de compra inválida, ela deve ser em formato de data!", "warning")
@@ -291,7 +296,8 @@ def edit_hardware():
             return redirect("/all_hardwares")
 
         # VALIDAR SE DT_PR_REV EXISTE E ESTÁ EM FORMATO DATE
-        elif dt_pr_rev and not is_date(dt_pr_rev):
+        # elif dt_pr_rev and not is_date(dt_pr_rev):
+        elif not validate_date(dt_pr_rev):
             cursor.close() 
             connection.close()
             flash("Data da próxima revisão inválida, ela deve ser em formato de data!", "warning")
@@ -309,12 +315,14 @@ def edit_hardware():
             return redirect("/all_hardwares")
 
         # validar se o valor inicital do ativo é possui 7 dígitos numéricos
-        elif price and not number_length(price, 8):
+        # elif price and not number_length(price, 8):
+        elif not validate_price(price, 8):
             flash("Preço inicial do ativo deve ser um número decimal positivo com no máximo 7 dígitos!", "warning")
             return redirect("/all_hardwares")
         
         #  VALIDAR SE O department ESTA DENTRO DA LISTA DE DEPARTAMENTOS DEFINIDA
-        elif department not in DEPARTMENTS_LIST or not department:
+        # elif department not in DEPARTMENTS_LIST or not department:
+        elif not is_in_list(department, DEPARTMENTS_LIST):
             flash("Departamento não encontrado", "danger")
             return redirect("/add_hardware")
         
@@ -361,7 +369,8 @@ def edit_hardware():
         hardware_id = request.args.get("hardware_id")
 
         # validar se o hardware_id existe, se é um digito e se é maior que 0
-        if not hardware_id or not hardware_id.isdigit() or int(hardware_id) <= 0:
+        # if not hardware_id or not hardware_id.isdigit() or int(hardware_id) <= 0:
+        if not validate_id(hardware_id):
             flash("4 Hardware não encontrado!", "danger")
             return redirect("/all_hardwares")
         
@@ -413,17 +422,20 @@ def add_rev_hardware():
             infos = ''
 
         # validar se o hardware_id existe, se é um digito e se é maior que 0, e se ele existe no bando de dados
-        if not hardware_id or not hardware_id.isdigit() or int(hardware_id) <= 0 or not hardware:
+        # if not hardware_id or not hardware_id.isdigit() or int(hardware_id) <= 0 or not hardware:
+        if not validate_id(hardware_id) or not hardware:
             flash("6 Hardware não encontrado!", "danger")
             return redirect('/all_hardwares')
 
         # validar se o tipo de rev está dentro da lista
-        elif type_rev not in REVISOES_HARDWARE or not type_rev:
+        # elif type_rev not in REVISOES_HARDWARE or not type_rev:
+        elif not is_in_list(type_rev, REVISOES_HARDWARE):
             flash("Tipo de revisão não encontrada!", "danger")
             return redirect('/all_hardwares')
 
         # VALIDAR SE A dt_buy EXISTE E SE ESTÁ NO FORMATO DE DATA
-        elif not dt_rev or not is_date(dt_rev):
+        # elif not dt_rev or not is_date(dt_rev):
+        elif not validate_date(dt_rev):
             flash("Informe a data da revisão!", "warning")
             return redirect('/all_hardwares')
         
@@ -435,7 +447,6 @@ def add_rev_hardware():
         
         #  VALIDAR SE O price POSSUI 7 DIGITOS NUMERICOS, SE É UM float E SE EXISTE
         elif not number_length(price, 8) or not is_decimal(price):
-        # elif not price or not number_length(price, 8):
             flash("Preço da revisão deve ser um número decimal positivo com no máximo 7 dígitos!", "warning")
             return redirect('/all_hardwares')
 
@@ -452,6 +463,12 @@ def add_rev_hardware():
             sql = "INSERT INTO revisoes_hardware (data, valor, tipo_revisao, infos_adicionais, hardwares_id) VALUES (%s, %s, %s, %s, %s)"
             data = (dt_rev, price, type_rev, infos, hardware_id)
             cursor.execute(sql,data) 
+
+            # alterar departamento caso o type_rev == "Mudança de departamento":
+            # if type_rev == "Mudança de departamento" and department and department in DEPARTMENTS_LIST:
+            if type_rev == "Mudança de departamento" and is_in_list(department, DEPARTMENTS_LIST):
+                cursor.execute("UPDATE hardwares SET departamento = %s WHERE id = %s", (department, hardware_id,))
+
             connection.commit()
             cursor.close() 
             connection.close()
@@ -465,7 +482,8 @@ def add_rev_hardware():
         hardware_id = request.args.get("hardware_id")
         
         # validar se o hardware_id existe, se é um digito e se é maior que 0
-        if not hardware_id or not hardware_id.isdigit() or int(hardware_id) <= 0:
+        # if not hardware_id or not hardware_id.isdigit() or int(hardware_id) <= 0:
+        if not validate_id(hardware_id):
             flash("7 Hardware não encontrado!", "danger")
             return redirect("/add_rev_hardware")
         
@@ -501,8 +519,6 @@ def all_rev_hardware():
     cursor.close()
     connection.close()
 
-    all_revs = results
-
     # converter a lista de tuplas em uma lista de listas
     all_revs = list(map(list, results))
 
@@ -510,12 +526,32 @@ def all_rev_hardware():
     for rev in all_revs:
         sum_revs += rev[2]
 
-    return render_template("all_rev_hardware.html", all_revs=all_revs, patrimony=patrimony, type_revs=REVISOES_HARDWARE, sum_revs=sum_revs)
+    return render_template("all_rev_hardware.html", all_revs=all_revs, patrimony=patrimony, sum_revs=sum_revs)
+
+
+@app.route("/all_soft_hardware")
+def all_soft_hardware():
+    hardware_id = request.args.get("hardware_id")
+    patrimony = request.args.get("patrimony")
+
+    # consultar todos as as revisões do hardware
+    connection = conectarBD("localhost", "root", "root", "empresa")
+    cursor = connection.cursor() 
+    cursor.execute("SELECT * FROM softwares WHERE hardwares_id = %s", (hardware_id,)) 
+    results = cursor.fetchall()
+    cursor.close()
+    connection.close()
+
+
+    # converter a lista de tuplas em uma lista de listas
+    all_softwares = list(map(list, results))
+    
+
+    return render_template("all_soft_hardware.html", all_softwares=all_softwares, patrimony=patrimony)
 
 
 
 # SOFTWARES
-# TODO
 @app.route("/add_software", methods=["POST","GET"])
 def add_software():
     if request.method == "POST":
@@ -529,40 +565,55 @@ def add_software():
 
         if not key:
             flash("Digite a chave de lincença!", "warning")
-            return render_template("add_software.html", departments=DEPARTMENTS_LIST)
+            return render_template("add_software.html")
         
         if not name:
             flash("Digite o nome do software!", "warning")
-            return render_template("add_software.html", departments=DEPARTMENTS_LIST)
+            return render_template("add_software.html")
         
         if not description:
             flash("Digite uma breve descrição do software!", "warning")
-            return render_template("add_software.html", departments=DEPARTMENTS_LIST)
+            return render_template("add_software.html")
         
         # VALIDAR SE A dt_buy EXISTE E SE ESTÁ NO FORMATO DE DATA
-        if not dt_buy or not is_date(dt_buy):
+        # if not dt_buy or not is_date(dt_buy):
+        elif not validate_date(dt_buy):
             flash("Informe a data de compra do ativo!", "warning")
-            return render_template("add_software.html", departments=DEPARTMENTS_LIST)
+            return render_template("add_software.html")
 
         # VALIDAR SE A dt_pr_rev EXISTE E SE ESTÁ NO FORMATO DE DATA
-        elif not dt_pr_rev or not is_date(dt_pr_rev):
+        # elif not dt_pr_rev or not is_date(dt_pr_rev):
+        elif not validate_date(dt_pr_rev):
             flash("Informe a próxima data de revisão do ativo!", "warning")
-            return render_template("add_software.html", departments=DEPARTMENTS_LIST)
+            return render_template("add_software.html")
         
         # VALIDAR SE dt_buy É DIFERENTE DE dt_pr_rev
         elif dt_buy == dt_pr_rev:
             flash("As datas de compra e revisão devem ser diferentes!", "warning")
-            return render_template("add_software.html", departments=DEPARTMENTS_LIST)
+            return render_template("add_software.html")
 
         # VALIDAR SE dt_buy É MENOR QUE dt_pr_rev
         elif dt_buy > dt_pr_rev:
             flash("A data da próxima revisão deve ser maior que a data de compra do ativo!", "warning")
-            return render_template("add_software.html", departments=DEPARTMENTS_LIST)
+            return render_template("add_software.html")
         
         #  VALIDAR SE O price POSSUI 7 DIGITOS NUMERICOS, SE É UM float E SE EXISTE
-        if not price or not number_length(price, 8):
+        # if not price or not number_length(price, 8) or not is_decimal(price):
+        elif not validate_price(price, 8):
             flash("Preço inicial do ativo deve ser um número decimal positivo com no máximo 7 dígitos!", "warning")
-            return render_template("add_hardware.html", departments=DEPARTMENTS_LIST)
+            return render_template("add_software.html")
+
+        # VALIDAR SE A CHAVE DE LINCEÇA NÃO EXISTE NO DB
+        connection = conectarBD("localhost", "root", "root", "empresa")
+        cursor = connection.cursor() 
+        cursor.execute("SELECT * FROM softwares WHERE chave_licenca = %s", (key,)) 
+        results = cursor.fetchall() 
+        cursor.close() 
+        connection.close()
+
+        if len(results) != 0:
+            flash("Chave de licença já cadastrada, tente outra!", "warning")
+            return redirect("/add_software")
 
 
         # VALIDAR SE O hardware ESCOLHIDO EXISTE NO DB
@@ -578,8 +629,10 @@ def add_software():
         for result in results:
             hardware_id_list.append(result[0])
 
-        if not hardware_id or int(hardware_id) not in hardware_id_list:
-            return render_template("mes.html", mes='nao encontrado')
+        # if not hardware_id or int(hardware_id) not in hardware_id_list:
+        if not validate_id(hardware_id) or not is_in_list(int(hardware_id), hardware_id_list):
+            flash("Patrimônio não encontrado!", "warning")
+            return redirect("/add_software")
 
     
         # INSERIR O SOFTWARE NO DB, JÁ QUE ELE PASSOU EM TODAS AS VALIDAÇÕES
@@ -616,12 +669,53 @@ def all_softwares():
         connection = conectarBD("localhost", "root", "root", "empresa")
         cursor = connection.cursor() 
 
-        cursor.execute("SELECT * FROM softwares") 
-        all_softwares = cursor.fetchall() 
+        cursor.execute("SELECT * FROM softwares JOIN hardwares ON softwares.hardwares_id = hardwares.id") 
+        results = cursor.fetchall() 
         cursor.close()     
         connection.close() 
+
+        all_softwares = list(map(list, results))
+        # return render_template("mes.html", mes=all_softwares)
+
         return render_template("all_softwares.html", all_softwares=all_softwares)
+
+
+@app.route("/delete_software", methods=["POST"])
+def delete_software():
+    software_id = request.form.get("software_id")
+
+    # validar se o software_id existe, se é um digito e se é maior que 0
+    if not validate_id(software_id):
+    # if not software_id or not software_id.isdigit() or int(software_id) <= 0:
+        flash("Software não encontrado!", "danger")
+        return redirect("/all_softwares")
     
+    # validar se o software escolhido está no banco de dados
+
+    connection = conectarBD("localhost", "root", "root", "empresa")
+    cursor = connection.cursor() 
+    cursor.execute("SELECT id FROM softwares WHERE id = %s", (software_id,))
+    results = cursor.fetchall()
+    cursor.close()
+    connection.close()
+
+
+    # validar se a quantidade de tuplas de resultado da busca no banco de dados é maior que 1, ou seja, se o dado existe
+    if len(results) != 1:
+        flash("Software não encontrado!", "danger")
+        return redirect("/all_softwares")
+
+    else:
+        connection = conectarBD("localhost", "root", "root", "empresa")
+        cursor = connection.cursor() 
+        cursor.execute("DELETE FROM softwares WHERE id = %s", (software_id,))
+        results = cursor.fetchall()
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+        flash("Software deletado", "success")
+        return redirect('/all_softwares') 
 
 if __name__ =='__main__':
     app.run()
---
+++ validations.py
@@ -0,0 +1,68 @@
+import datetime
+from decimal import Decimal, InvalidOperation
+import re
+
+# VALIDAR SE O VALOR É DECIMAL
+def is_decimal(n):
+    try:
+        Decimal(n)
+        return True
+    except InvalidOperation:
+        return False
+
+# VALIDAR SE O VALOR É FORMATO DATA
+def is_date(value):
+    date_regex = re.compile(r'^\d{4}-\d{2}-\d{2}$')
+
+    if date_regex.match(value):
+        return True
+    else:
+        return False
+
+# VALIDAR SE POSSUI n DÍGITOS
+def number_length(number, n):
+    # converte o numero em string
+    number_str = str(number)
+    # encontra o tamanho da string e subtrai 1 para desconsiderar o ponto decimal
+    number_length = len(number_str)
+    # se o tamanho for maior que n ou menor ou igual que 0, então retorna false
+    if number_length > n or number_length <= 0:
+        return False
+    else:
+        return True 
+    
+# VALIADR SE O VALOR EXISTE E ESTA EM FORMATO DE DATA
+def validate_date(value):
+    if value and is_date(value):
+        return True
+    else:
+        return False
+
+# VALIADR SE O VALOR EXISTE, POSSUI n DÍGITOS (virgula conta, por isso tem que ser um a mais) E É DECIMAL
+def validate_price(value, n):
+    if value and number_length(value, n) and is_decimal(value):
+        return True
+    else:
+        return False
+    
+# VALIDAR SE O VALOR SE ENCONTRA DENTRO DE UMA LISTA
+def is_in_list(value, list):
+    if value and value in list:
+        return True
+    else:
+        return False
+    
+# VALIDAR SE O VALOR É UM DÍGITO, SE EXISTE E SE É MAIOR QUE ZERO
+def validate_id(value):
+    if value and value.isdigit() and int(value) > 0:
+        return True
+    else:
+        return False
+    
+# VALIDAR SE VALOR NUMÉRICO, SE EXISTE E SE POSSUI n DÍGITOS
+def validate_patrimony(value, n):
+    if value and number_length(value, n) and value.isnumeric():
+        return True
+    else:
+        return False
+    
